319a3129a813c4664555edae99512c7824715ce6
Pankaj Ojha <pankaj250483@gmail.com>
2024-05-15 17:01:09 +0530

    updt

--- admin/src/components/UserAccess.js
+++ admin/src/components/UserAccess.js
@@ -3,16 +3,18 @@ import { useParams, Link, useNavigate } from 'react-router-dom';
 import { ref, get } from 'firebase/database'; 
 import { database } from '../firebaseconfig';
 import ToggleButton from './ToggleButton';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const UserAccess = () => {
   const { userId } = useParams();
   let navigate = useNavigate();  
-  const [userData, setUserData] = useState('');
+  const [userData, setUserData] = useState({});
   const [errorMessage, setErrorMessage] = useState('');
   const applicationtypes = ['Community Watch', 'Report', 'Checkpoint Tour','Payroll','Attendance'];
+  const viewAccess = ['Limited View Access', 'Full View Access', 'Edit Access'];
   const [reportTemplates, setReportTemplates] = useState([]);
 
-  
   useEffect(() => {
     if (userId) {
       const reportTemplateRef = ref(database, `users/${userId}`);
@@ -31,29 +33,31 @@ const UserAccess = () => {
     }
   }, [userId]);
 
-  const reportTemplatesRef = ref(database, 'reportTemplates');
-
-  // Fetch data from Firebase Realtime Database
-  get(reportTemplatesRef)
-    .then((snapshot) => {
-      if (snapshot.exists()) {
-        const data = snapshot.val();
-        // Convert the object of objects into an array of objects
-        const reportTemplateArray = Object.keys(data).map(key => ({
-          id: key,
-          name: data[key].name,
-        }));
-        setReportTemplates(reportTemplateArray);
-      } else {
-        setErrorMessage('No report templates found');
-      }
-    })
-    .catch((error) => {
-      setErrorMessage(`Error fetching report templates: ${error.message}`);
-    });
+  useEffect(() => {
+    const reportTemplatesRef = ref(database, 'reportTemplates');
+    get(reportTemplatesRef)
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const data = snapshot.val();
+          const reportTemplateArray = Object.keys(data).map(key => ({
+            id: key,
+            name: data[key].name,
+          }));
+          setReportTemplates(reportTemplateArray);
+        } else {
+          setErrorMessage('No report templates found');
+        }
+      })
+      .catch((error) => {
+        setErrorMessage(`Error fetching report templates: ${error.message}`);
+      });
+  }, []);
 
   const handleChange = (field, value) => {
-    userData[field]= value;
+    setUserData(prevData => ({
+      ...prevData,
+      [field]: value
+    }));
   };
 
   return (
@@ -62,68 +66,109 @@ const UserAccess = () => {
         <div className="col-md-8 py-4" style={{ color: '#735744', height: '680px' }}>
           <div className="text-center">
             <Link to="/home">
-              <button className="btn btn-danger mb-4 rounded-pill px-5 "><h3>ADMINISTRATOR PORTAL</h3></button>
+              <button className="btn btn-danger mb-4 rounded-pill px-5 ">
+                <h3>ADMINISTRATOR PORTAL</h3>
+              </button>
             </Link>
           </div>
           <div className="text-center">
             <Link to="/UserList">
-            <h3 className="btn btn-danger mb-4 rounded-pill px-5 ">Assign/Edit User UserAccess</h3>
+              <h3 className="btn btn-danger mb-4 rounded-pill px-5 ">Assign/Edit User UserAccess</h3>
             </Link>
           </div>
           <div className="text-center">
             <table className='table'>
+              <thead>
                 <tr>
-                <th className='btn-danger rounded-pill'>User Type</th>
-                <th className='btn-danger rounded-pill'>User Email</th>
-                <th className='btn-danger rounded-pill'>Full Name</th>
-                <th className='btn-danger rounded-pill'>Company Name</th>
-                <th className='btn-danger rounded-pill'>Location</th>
+                  <th className='btn-danger rounded-pill'>User Type</th>
+                  <th className='btn-danger rounded-pill'>User Email</th>
+                  <th className='btn-danger rounded-pill'>Full Name</th>
+                  <th className='btn-danger rounded-pill'>Company Name</th>
+                  <th className='btn-danger rounded-pill'>Location</th>
                 </tr>
+              </thead>
+              <tbody>
                 <tr>
-                    <td>
+                  <td>
                     <select
-                        className="form-control btn-danger rounded my-1 text-center w-100"
-                        value={userData.type}
-                        onChange={(e) => handleChange('type', e.target.value)} >
-                        <option value="Client">Client</option>
-                        <option value="Security">Security</option>
-                        <option value="Management">Management</option>
-                        <option value="Administrator">Administrator</option>
+                      className="form-control btn-danger rounded my-1 text-center w-100"
+                      value={userData.type || ''}
+                      onChange={(e) => handleChange('type', e.target.value)}
+                    >
+                      <option value="Client">Client</option>
+                      <option value="Security">Security</option>
+                      <option value="Management">Management</option>
+                      <option value="Administrator">Administrator</option>
                     </select>
-                    </td>
-                    <td>{userData.email}</td>
-                    <td>{userData.name} {userData.lname}</td>
-                    <td>{userData.company}</td>
-                    <td>{userData.companyAdress}</td>
+                  </td>
+                  <td>{userData.email}</td>
+                  <td>{userData.name} {userData.lname}</td>
+                  <td>{userData.company}</td>
+                  <td>{userData.companyAdress}</td>
                 </tr>
+              </tbody>
             </table>
           </div>
-        <div>
-            <h6>Click <b>Report Name</b> to provide access to user {userData.email} :</h6>
-            <div className="d-flex flex-wrap">
-          {reportTemplates.map((reportTemplate) => (
-            <div key={reportTemplate.id} className="m-2">
-              <ToggleButton permission="Report" name={reportTemplate.name} reportid={reportTemplate.id} userId= {userId}/>
-            </div>
-          ))}
-          </div>
-          <hr/>
-        </div>
 
-        <div>
-            <h6>Click <b>Application Type </b> to provide access to user {userData.email} :</h6>
-            <div className="d-flex flex-wrap">
-          {applicationtypes.map((applicationtype) => (
-            <div key={applicationtype} className="m-2">
-              <ToggleButton permission="ApplicationType" name={applicationtype} reportid={applicationtype} userId= {userId}/>
+          <div className="accordion" id="accordionLocations">
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingAccess">
+                <button className="accordion-button accordion-button1 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseAccess" aria-expanded="false" aria-controls="collapseAccess">
+                  View Access
+                </button>
+              </h2>
+              <div id="collapseAccess" className="accordion-collapse collapse" aria-labelledby="headingAccess" data-bs-parent="#accordionLocations">
+                <div className="accordion-body">
+                  <div className="d-flex flex-wrap">
+                    {viewAccess.map((item) => (
+                      <div key={item} className="m-2">
+                        <ToggleButton permission="ApplicationType" name={item} reportid={item} userId={userId} />
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
             </div>
-          ))}
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingReports">
+                <button className="accordion-button accordion-button1 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseReports" aria-expanded="false" aria-controls="collapseReports">
+                  Report Templates
+                </button>
+              </h2>
+              <div id="collapseReports" className="accordion-collapse collapse" aria-labelledby="headingReports" data-bs-parent="#accordionLocations">
+                <div className="accordion-body">
+                  <div className="d-flex flex-wrap">
+                    {reportTemplates.map((reportTemplate) => (
+                      <div key={reportTemplate.id} className="m-2">
+                        <ToggleButton permission="Report" name={reportTemplate.name} reportid={reportTemplate.id} userId={userId} />
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingApplicationTypes">
+                <button className="accordion-button accordion-button1 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseApplicationTypes" aria-expanded="false" aria-controls="collapseApplicationTypes">
+                  Application Type
+                </button>
+              </h2>
+              <div id="collapseApplicationTypes" className="accordion-collapse collapse" aria-labelledby="headingApplicationTypes" data-bs-parent="#accordionLocations">
+                <div className="accordion-body">
+                  <div className="d-flex flex-wrap">
+                    {applicationtypes.map((item) => (
+                      <div key={item} className="m-2">
+                        <ToggleButton permission="ApplicationType" name={item} reportid={item} userId={userId} />
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <hr/>
-        </div>
 
+        </div>
       </div>
-    </div>
     </div>
   );
 }
